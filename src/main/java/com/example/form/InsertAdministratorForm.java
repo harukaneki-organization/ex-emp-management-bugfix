474e4dd2618efd932d7e22a7e6a1edb7a4930571
nooum39 <mikantaruto1380@gmail.com>
2024-06-07 15:28:40 +0900

    1-5 中級確認用パスワード欄を作成

--- src/main/java/com/example/form/InsertAdministratorForm.java
+++ src/main/java/com/example/form/InsertAdministratorForm.java
@@ -22,6 +22,18 @@ public class InsertAdministratorForm {
 	/** パスワード */
 	private String password;
 
+	@NotBlank(message = "確認パスワードを入力してください")
+	/** パスワード */
+	private String confirmationPassword;
+
+	public String getConfirmationPassword() {
+		return confirmationPassword;
+	}
+
+	public void setConfirmationPassword(String confirmationPassword) {
+		this.confirmationPassword = confirmationPassword;
+	}
+
 	public String getName() {
 		return name;
 	}
@@ -48,8 +60,11 @@ public class InsertAdministratorForm {
 
 	@Override
 	public String toString() {
-		return "InsertAdministratorForm [name=" + name + ", mailAddress=" + mailAddress + ", password=" + password
-				+ "]";
+		return "InsertAdministratorForm{" +
+				"name='" + name + '\'' +
+				", mailAddress='" + mailAddress + '\'' +
+				", password='" + password + '\'' +
+				", confirmationPassword='" + confirmationPassword + '\'' +
+				'}';
 	}
-
 }
